ab816de447d54df211f73df6e8a03f76afc04ac4
Alex Jercan <jercan_alex27@yahoo.com>
2023-06-27 14:06:18 +0300

    Initial Commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import type { VoidComponent } from "solid-js";
+
+const Home: VoidComponent = () => {
+    return (
+        <main class="mx-auto p-4 text-center text-gray-700">
+      Hello
+        </main>
+    );
+};
+
+export default Home;
